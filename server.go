8938c728e016f28652b26ab58725543dfceafb65
Paweł Zając <pavlzayac@gmail.com>
2024-09-29 21:39:42 +0200

    Update net/http usage for gin

--- server.go
+++ server.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gin-gonic/gin"
 	"github.com/pavozayac/constraint-service/graph"
 )
 
@@ -20,9 +20,16 @@ func main() {
 
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		playground.Handler("GraphQL playground", "/query")(c.Writer, c.Request)
+	})
+
+	r.POST("/query", func(c *gin.Context) {
+		srv.ServeHTTP(c.Writer, c.Request)
+	})
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(r.Run())
 }
