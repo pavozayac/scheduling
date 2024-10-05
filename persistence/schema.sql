51d542761ad4081cd1f61c1de8517d526219c0b2
Paweł Zając <pavlzayac@gmail.com>
2024-10-05 09:47:26 +0200

    Improve sql schema

--- persistence/schema.sql
+++ persistence/schema.sql
@@ -1,32 +1,32 @@
 CREATE TABLE schedules (
-    id integer GENERATED ALWAYS AS IDENTITY,
-    title VARCHAR NOT NULL,
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title varchar(200) NOT NULL,
 );
 
 CREATE TABLE workers (
-    id integer GENERATED ALWAYS AS IDENTITY,
-    title VARCHAR NOT NULL,
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title varchar(200) NOT NULL,
     schedule_id integer REFERENCES schedules(id),
 );
 
 CREATE TABLE tasks (
-    id integer GENERATED ALWAYS AS IDENTITY,
-    title VARCHAR NOT NULL,
-    story TEXT NOT NULL,
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title varchar(200) NOT NULL,
+    story varchar(1000) NOT NULL,
     schedule_id integer REFERENCES schedules(id),
 );
 
 CREATE TABLE locations (
-    id integer GENERATED ALWAYS AS IDENTITY,
-    title VARCHAR NOT NULL,
-    story TEXT NOT NULL,
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title varchar(200) NOT NULL,
+    story text(1000) NOT NULL,
     schedule_id integer REFERENCES schedules(id),
 );
 
 CREATE TYPE constraint_type as ENUM ('must', 'cant');
 
 CREATE TABLE worker_time_constraints (
-    id integer GENERATED ALWAYS AS IDENTITY,
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     worker_id integer REFERENCES workers(id);
     start_slot integer NOT NULL;
     end_slot integer NOT NULL;
@@ -34,7 +34,7 @@ CREATE TABLE worker_time_constraints (
 );
 
 CREATE TABLE location_time_constraints (
-    id integer GENERATED ALWAYS AS IDENTITY,
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     location_id integer REFERENCES locations(id);
     start_slot integer NOT NULL;
     end_slot integer NOT NULL;
@@ -42,7 +42,7 @@ CREATE TABLE location_time_constraints (
 );
 
 CREATE TABLE task_time_constraints (
-    id integer GENERATED ALWAYS AS IDENTITY,
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     task_id integer REFERENCES tasks(id);
     start_slot integer NOT NULL;
     end_slot integer NOT NULL;
@@ -50,14 +50,14 @@ CREATE TABLE task_time_constraints (
 );
 
 CREATE TABLE worker_task_constraints (
-    id integer GENERATED ALWAYS AS IDENTITY,
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     worker_id integer REFERENCES workers(id);
     task_id integer REFERENCES tasks(id);
     kind constraint_type NOT NULL;
 );
 
 CREATE TABLE location_task_constraints (
-    id integer GENERATED ALWAYS AS IDENTITY,
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     location_id integer REFERENCES locations(id);
     task_id integer REFERENCES tasks(id);
     kind constraint_type NOT NULL;
