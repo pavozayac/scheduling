d12acd88d76319b6d69d8a763e4a3503c316617c
Paweł Zając <pavlzayac@gmail.com>
2024-10-06 00:52:45 +0200

    Chore: run tidy

--- tools.go
+++ tools.go
@@ -1,8 +1,10 @@
 //go:build tools
+// +build tools
 
 package tools
 
 import (
 	_ "github.com/99designs/gqlgen"
 	_ "github.com/99designs/gqlgen/graphql/introspection"
+	_ "github.com/pressly/goose/v3/cmd/goose"
 )
