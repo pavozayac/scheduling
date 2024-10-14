f52b927f41a1b69ebece883292bd40d42ef3cd95
Paweł Zając <pavlzayac@gmail.com>
2024-10-14 21:58:24 +0200

    Fix schema errors

--- src/constraint-service/internal/infrastructure/db/migrations/00001_init_tables.sql
+++ src/constraint-service/internal/infrastructure/db/migrations/00001_init_tables.sql
@@ -2,67 +2,67 @@
 
 CREATE TABLE schedules (
     id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    title varchar(200) NOT NULL,
+    title varchar(200) NOT NULL
 );
 
 CREATE TABLE workers (
     id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     title varchar(200) NOT NULL,
-    schedule_id integer REFERENCES schedules(id),
+    schedule_id integer REFERENCES schedules(id) NOT NULL
 );
 
 CREATE TABLE tasks (
     id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     title varchar(200) NOT NULL,
     story varchar(1000) NOT NULL,
-    schedule_id integer REFERENCES schedules(id),
+    schedule_id integer REFERENCES schedules(id) NOT NULL
 );
 
 CREATE TABLE locations (
     id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     title varchar(200) NOT NULL,
     story text(1000) NOT NULL,
-    schedule_id integer REFERENCES schedules(id),
+    schedule_id integer REFERENCES schedules(id) NOT NULL
 );
 
 CREATE TYPE constraint_type as ENUM ('must', 'cant');
 
 CREATE TABLE worker_time_constraints (
     id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    worker_id integer REFERENCES workers(id);
-    start_slot integer NOT NULL;
-    end_slot integer NOT NULL;
-    kind constraint_type NOT NULL;
+    worker_id integer REFERENCES workers(id) NOT NULL,
+    start_slot integer NOT NULL,
+    end_slot integer NOT NULL,
+    kind constraint_type NOT NULL
 );
 
 CREATE TABLE location_time_constraints (
     id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    location_id integer REFERENCES locations(id);
-    start_slot integer NOT NULL;
-    end_slot integer NOT NULL;
-    kind constraint_type NOT NULL;
+    location_id integer REFERENCES locations(id) NOT NULL,
+    start_slot integer NOT NULL,
+    end_slot integer NOT NULL,
+    kind constraint_type NOT NULL
 );
 
 CREATE TABLE task_time_constraints (
     id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    task_id integer REFERENCES tasks(id);
-    start_slot integer NOT NULL;
-    end_slot integer NOT NULL;
-    kind constraint_type NOT NULL;
+    task_id integer REFERENCES tasks(id) NOT NULL,
+    start_slot integer NOT NULL,
+    end_slot integer NOT NULL,
+    kind constraint_type NOT NULL
 );
 
 CREATE TABLE worker_task_constraints (
     id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    worker_id integer REFERENCES workers(id);
-    task_id integer REFERENCES tasks(id);
-    kind constraint_type NOT NULL;
+    worker_id integer REFERENCES workers(id) NOT NULL,
+    task_id integer REFERENCES tasks(id) NOT NULL,
+    kind constraint_type NOT NULL
 );
 
 CREATE TABLE location_task_constraints (
     id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    location_id integer REFERENCES locations(id);
-    task_id integer REFERENCES tasks(id);
-    kind constraint_type NOT NULL;
+    location_id integer REFERENCES locations(id),
+    task_id integer REFERENCES tasks(id) NOT NULL,
+    kind constraint_type NOT NULL
 );
 
 -- +goose Down
