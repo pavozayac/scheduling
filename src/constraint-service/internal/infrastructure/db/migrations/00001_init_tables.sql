7eefe6a8b938c85016abc49ab7122dd9304b385f
Paweł Zając <pavlzayac@gmail.com>
2024-10-15 12:47:58 +0200

    Update sql according to model changes

--- src/constraint-service/internal/infrastructure/db/migrations/00001_init_tables.sql
+++ src/constraint-service/internal/infrastructure/db/migrations/00001_init_tables.sql
@@ -27,42 +27,14 @@ CREATE TABLE locations (
 
 CREATE TYPE constraint_type as ENUM ('must', 'cant');
 
-CREATE TABLE worker_time_constraints (
-    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    worker_id integer REFERENCES workers(id) NOT NULL,
-    start_slot integer NOT NULL,
-    end_slot integer NOT NULL,
-    kind constraint_type NOT NULL
-);
-
-CREATE TABLE location_time_constraints (
-    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    location_id integer REFERENCES locations(id) NOT NULL,
-    start_slot integer NOT NULL,
-    end_slot integer NOT NULL,
-    kind constraint_type NOT NULL
-);
-
-CREATE TABLE task_time_constraints (
-    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    task_id integer REFERENCES tasks(id) NOT NULL,
-    start_slot integer NOT NULL,
-    end_slot integer NOT NULL,
-    kind constraint_type NOT NULL
-);
-
-CREATE TABLE worker_task_constraints (
-    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    worker_id integer REFERENCES workers(id) NOT NULL,
-    task_id integer REFERENCES tasks(id) NOT NULL,
-    kind constraint_type NOT NULL
-);
-
-CREATE TABLE location_task_constraints (
-    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE constraints (
     location_id integer REFERENCES locations(id),
-    task_id integer REFERENCES tasks(id) NOT NULL,
-    kind constraint_type NOT NULL
+    task_id integer REFERENCES tasks(id),
+    worker_id integer REFERENCES workers(id),
+    start_slot integer,
+    end_slot integer,
+    kind constraint_type NOT NULL,
+    UNIQUE (location_id, task_id, worker_id, start_slot, end_slot, kind)
 );
 
 -- +goose Down
@@ -72,8 +44,4 @@ DROP TABLE workers;
 DROP TABLE tasks;
 DROP TABLE locations;
 DROP TYPE constraint_type;
-DROP TABLE worker_time_constraints;
-DROP TABLE location_time_constraints;
-DROP TABLE task_time_constraints;
-DROP TABLE worker_task_constraints;
-DROP TABLE location_task_constraints;
+DROP TABLE constraints;
--- src/constraint-service/internal/infrastructure/db/query.sql
+++ src/constraint-service/internal/infrastructure/db/query.sql
@@ -1,5 +1,35 @@
 -- name: GetSchedule :one
 SELECT * from schedules WHERE id = $1;
 
+-- name: GetAllSchedules :many
+SELECT * from schedules;
+
 -- name: GetWorker :one
 SELECT * from workers WHERE id = $1;
+
+-- name: GetAllWorkers :many
+SELECT * from workers;
+
+-- name: GetTask :one
+SELECT * from tasks WHERE id = $1;
+
+-- name: GetAllTasks :many
+SELECT * from tasks;
+
+-- name: GetLocation :one
+SELECT * from locations WHERE id = $1;
+
+-- name: GetAllLocations :many
+SELECT * from locations;
+
+-- name: GetConstraint :one
+SELECT * from constraints WHERE location_id = $1 AND task_id = $2 AND worker_id = $3 AND start_slot = $4 AND end_slot = $5 AND kind = $6;
+
+-- name: GetAllConstraintsForTask :many
+SELECT * from constraints WHERE task_id = $1;
+
+-- name: GetAllConstraintsForLocation :many
+SELECT * from constraints WHERE location_id = $1;
+
+-- name: GetAllConstraintsForWorker :many
+SELECT * from constraints WHERE worker_id = $1;
