ae5b06e8599e6c0d03dbd8e0b52129554da186e7
Paweł Zając <pavlzayac@gmail.com>
2024-10-21 12:56:44 +0200

    Update schema with uuid, make minor improvements

--- src/constraint-service/internal/infrastructure/db/migrations/00001_init_tables.sql
+++ src/constraint-service/internal/infrastructure/db/migrations/00001_init_tables.sql
@@ -1,37 +1,37 @@
 -- +goose Up
 
 CREATE TABLE schedules (
-    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id uuid PRIMARY KEY,
     title varchar(200) NOT NULL
 );
 
 CREATE TABLE workers (
-    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id uuid PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     title varchar(200) NOT NULL,
-    schedule_id integer REFERENCES schedules(id) NOT NULL
+    schedule_id uuid REFERENCES schedules(id) NOT NULL
 );
 
 CREATE TABLE tasks (
-    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id uuid PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     title varchar(200) NOT NULL,
     story varchar(1000) NOT NULL,
-    schedule_id integer REFERENCES schedules(id) NOT NULL
+    schedule_id uuid REFERENCES schedules(id) NOT NULL
 );
 
 CREATE TABLE locations (
-    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id uuid PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     title varchar(200) NOT NULL,
-    story text(1000) NOT NULL,
-    schedule_id integer REFERENCES schedules(id) NOT NULL
+    story varchar(1000) NOT NULL,
+    schedule_id uuid REFERENCES schedules(id) NOT NULL
 );
 
-CREATE TYPE constraint_type as ENUM ('must', 'cant');
+CREATE TYPE constraint_type as ENUM ('must', 'cannot');
 
 CREATE TABLE constraints (
-    schedule_id integer REFERENCES schedules(id),
-    location_id integer REFERENCES locations(id),
-    task_id integer REFERENCES tasks(id),
-    worker_id integer REFERENCES workers(id),
+    schedule_id uuid REFERENCES schedules(id),
+    location_id uuid REFERENCES locations(id),
+    task_id uuid REFERENCES tasks(id),
+    worker_id uuid REFERENCES workers(id),
     start_slot integer,
     end_slot integer,
     kind constraint_type NOT NULL,
@@ -40,9 +40,9 @@ CREATE TABLE constraints (
 
 -- +goose Down
 
-DROP TABLE schedules;
-DROP TABLE workers;
-DROP TABLE tasks;
-DROP TABLE locations;
-DROP TYPE constraint_type;
 DROP TABLE constraints;
+DROP TABLE locations;
+DROP TABLE tasks;
+DROP TABLE workers;
+DROP TABLE schedules;
+DROP TYPE constraint_type;
