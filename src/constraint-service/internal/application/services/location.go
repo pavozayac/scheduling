4bb440338a9f12ddb7a0d72b4917f43e7cfbf210
Paweł Zając <pavlzayac@gmail.com>
2024-12-07 12:41:26 +0100

    Add service implementations

---
+++ src/constraint-service/internal/application/services/location.go
@@ -0,0 +1,75 @@
+package services
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/pavozayac/scheduling/src/constraint-service/internal/domain/model"
+	"github.com/pavozayac/scheduling/src/constraint-service/internal/domain/ports"
+	"github.com/pavozayac/scheduling/src/constraint-service/internal/domain/shared"
+)
+
+type locationService struct {
+	repo ports.LocationRepository
+}
+
+func NewLocationService(repo ports.LocationRepository) *locationService {
+	return &locationService{repo: repo}
+}
+
+func (s *locationService) CreateOrModifyLocation(ctx context.Context, dto ports.LocationDTO) (*ports.LocationDTO, error) {
+	var id, scheduleId shared.Identity
+
+	if dto.Id == "" {
+		id = shared.UuidGenerator{}.Generate()
+	} else {
+		id.Scan(dto.Id)
+	}
+
+	scheduleId.Scan(dto.ScheduleId)
+
+	location, err := model.NewLocation(id, scheduleId, dto.Name, dto.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	err = s.repo.SaveOrUpdateLocation(ctx, *location)
+	if err != nil {
+		return nil, err
+	}
+
+	feedback, err := s.GetLocation(ctx, dto.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	return feedback, nil
+}
+
+func (s *locationService) GetLocation(ctx context.Context, id string) (*ports.LocationDTO, error) {
+	var locationId shared.Identity
+	locationId.Scan(id)
+
+	location, err := s.repo.GetLocation(ctx, locationId)
+	if err != nil {
+		return nil, err
+	}
+
+	result := &ports.LocationDTO{
+		Id:          uuid.UUID(location.Id()).String(),
+		ScheduleId:  uuid.UUID(location.ScheduleId()).String(),
+		Name:        location.Name(),
+		Description: location.Description(),
+	}
+
+	return result, nil
+}
+
+func (s *locationService) RemoveLocation(ctx context.Context, id string) error {
+	var locationId shared.Identity
+	locationId.Scan(id)
+
+	return s.repo.DeleteLocation(ctx, locationId)
+}
+
+var _ ports.LocationService = &locationService{}
---
+++ src/constraint-service/internal/application/services/task.go
@@ -0,0 +1,73 @@
+package services
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/pavozayac/scheduling/src/constraint-service/internal/domain/model"
+	"github.com/pavozayac/scheduling/src/constraint-service/internal/domain/ports"
+	"github.com/pavozayac/scheduling/src/constraint-service/internal/domain/shared"
+)
+
+type taskService struct {
+	repo ports.TaskRepository
+}
+
+func NewTaskService(repo ports.TaskRepository) *taskService {
+	return &taskService{repo: repo}
+}
+
+func (s *taskService) CreateOrModifyTask(ctx context.Context, dto ports.TaskDTO) (*ports.TaskDTO, error) {
+	var id, scheduleId shared.Identity
+
+	if dto.Id == "" {
+		id = shared.UuidGenerator{}.Generate()
+	} else {
+		id.Scan(dto.Id)
+	}
+
+	scheduleId.Scan(dto.ScheduleId)
+
+	task, err := model.NewTask(id, scheduleId, dto.Title, dto.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	err = s.repo.SaveOrUpdateTask(ctx, *task)
+	if err != nil {
+		return nil, err
+	}
+
+	feedback, err := s.GetTask(ctx, dto.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	return feedback, nil
+}
+
+func (s *taskService) GetTask(ctx context.Context, id string) (*ports.TaskDTO, error) {
+	var taskId shared.Identity
+	taskId.Scan(id)
+
+	task, err := s.repo.GetTask(ctx, taskId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ports.TaskDTO{
+		Id:          uuid.UUID(task.Id()).String(),
+		ScheduleId:  uuid.UUID(task.ScheduleId()).String(),
+		Title:       task.Name(),
+		Description: task.Description(),
+	}, nil
+}
+
+func (s *taskService) RemoveTask(ctx context.Context, id string) error {
+	var taskId shared.Identity
+	taskId.Scan(id)
+
+	return s.repo.DeleteTask(ctx, taskId)
+}
+
+var _ ports.TaskService = &taskService{}
